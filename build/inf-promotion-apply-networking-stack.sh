65a203b90d9b521403754d16e6baa140e71188e0
keith <keithgair@yahoo.co.uk>
2017-07-05 12:18:05 +0100

    Fixed environments scripts

--- build/inf-promotion-apply-networking-stack.sh
+++ build/inf-promotion-apply-networking-stack.sh
@@ -1,11 +1,12 @@
 tf_repo_url=$1
-stack_name=$2
-environment=$3
-tag=$4
+stage_name=$2
+environment_name=$3
+stack_name=$4
+tag=$5
 
 git clone ${tf_repo_url} tf_repo
 
-cd tf_infrastructure/${environment}/${stack_name}
+cd tf_repo/environments/${stage_name}/${environment_name}/${stack_name}
 
 git checkout tags/${tag}
 echo "terraform apply would happen here..."
