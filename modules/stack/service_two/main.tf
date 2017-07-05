3bd265827dbae001c907b8615af08ab9a1532344
keith <keithgair@yahoo.co.uk>
2017-07-05 09:07:15 +0100

    Corrected variables in service modules

--- modules/stack/service_two/main.tf
+++ modules/stack/service_two/main.tf
@@ -4,8 +4,8 @@ module "microservice" {
     service_name = "${var.service_name}"
     service_port = "${var.service_port}"
     service_image_id = "${var.service_image_id}"
-    vpc_name = "${var.vpc.name}"
-    vpc_id = "${var.vpc.id}"
+    vpc_name = "${var.vpc_name}"
+    vpc_id = "${var.vpc_id}"
     internal_subnet_id = "${var.internal_subnet_id}"
     external_subnet_id = "${var.external_subnet_id}"
 }
